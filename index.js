59e7b206b3549fb742f072b8b7533910872c4cee
James Halliday <mail@substack.net>
2014-07-01 03:22:22 -0700

    opts.modeReset

--- index.js
+++ index.js
@@ -8,11 +8,39 @@ module.exports = function (opts, fn) {
         opts = {};
     }
     if (!opts) opts = {};
+    
+    var colors = null, reset = null;
+    
+    if (opts.modeReset === true) {
+        // wow hacky
+        var prevLog = console.log;
+        var prevError = console.error;
+        reset = function () {
+            console.log = prevLog;
+            console.error = prevError;
+        };
+        
+        console.log = function () {
+            process.stdout.write('\x1b[0m');
+            var res = prevLog.apply(this, arguments);
+            process.stdout.write(colors && colors.mode || '');
+            return res;
+        };
+        console.error = function () {
+            process.stderr.write('\x1b[0m');
+            var res = prevError.apply(this, arguments);
+            process.stderr.write(colors && colors.mode || '');
+            return res;
+        };
+    }
+     
     return function (args, cb) {
         var test = tape.createHarness();
         test(function (t) { fn(args, t) });
         var stream = test.createStream();
+        colors = colorize(opts);
+        if (reset) stream.once('end', function () { reset() });
         stream.pipe(parser(function (results) { cb(results.ok) }));
-        return stream.pipe(colorize(opts));
+        return stream.pipe(colors);
     };
 };
