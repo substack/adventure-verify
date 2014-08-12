62a82e9db1cd11222d19e49a79e1d99bcf7359a2
James Halliday <mail@substack.net>
2014-08-11 22:54:08 -0700

    coverage for modeReset

--- test/robots.js
+++ test/robots.js
@@ -10,3 +10,14 @@ test('robots', function (t) {
         t.notOk(ok);
     });
 });
+
+test('rset', function (t) {
+    t.plan(2);
+    var p = require('./robots/rset.js');
+    p.verify(['555'], function (ok) {
+        t.ok(ok);
+    });
+    p.verify(['556'], function (ok) {
+        t.notOk(ok);
+    });
+});
