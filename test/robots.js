0e5d40dc76548cd4b8c8ecbd1336bb64f2c095e7
James Halliday <mail@substack.net>
2014-08-11 22:51:30 -0700

    test

---
+++ test/robots.js
@@ -0,0 +1,12 @@
+var test = require('tape');
+
+test('robots', function (t) {
+    t.plan(2);
+    var p = require('./robots/beep_boop.js');
+    p.verify(['555'], function (ok) {
+        t.ok(ok);
+    });
+    p.verify(['556'], function (ok) {
+        t.notOk(ok);
+    });
+});
